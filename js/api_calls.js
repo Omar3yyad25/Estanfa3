37161b5c740fe481ec6efc999d6d45afc8179d63
Omar W. Ayyad <s-omar.ayyad@zewailcity.edu.eg>
2023-05-25 01:56:52 +0300

    sss

--- js/api_calls.js
+++ js/api_calls.js
@@ -1,10 +1,11 @@
 export async function get({ endpoint = "", id = "" }) {
   try {
     const response = await fetch(`http://estanfa3.com:8443/${endpoint}/${id}`, {
-      mode: "no-cors",
+      mode: "cors",
       credentials: "same-origin",
       headers: {
         'Content-Type': 'application/json',
+        'Origin': 'http://estanfa3.com'
       }
     });
     console.log(response);
