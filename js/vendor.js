3bf1f5848d2e824a87282bd72ef70c096240ac04
Omar W. Ayyad <s-omar.ayyad@zewailcity.edu.eg>
2023-05-29 17:40:07 +0300

    ff

--- js/vendor.js
+++ js/vendor.js
@@ -42,7 +42,7 @@ function create_offers(offer) {
   <td><span>${offer.description}</span></td>
   <td><span>${offer.tradedProductId}</span></td>
   <td><span>${offer.offeredPrice}</span></td>
-  <td><span><a >Accept</a></span></td>
+  <td><span><a href ="http://estanfa3.com:8443/createcontract/${offer.tradedProductId}" >Accept</a></span></td>
   <td><span><a href="http://estanfa3.com:8443/deleteoffer/${offer.id}">Decline</a></span></td>
 `
   const offers = document.getElementById("load_offer");
